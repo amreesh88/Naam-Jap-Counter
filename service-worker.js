aed88b454e1abd1f5cc97c8da9b25769bf9c0253
amreesh88 <amrishbilgaiyan7@gmail.com>
2026-04-27 12:51:18 +0530

    added pwa files

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "naam-app-v2";
+const urlsToCache = [
+  "index.html",
+  "style.css",
+  "script.js",
+  "icon.svg"
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
